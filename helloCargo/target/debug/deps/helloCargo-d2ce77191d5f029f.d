308d77fba3d8d1b7758f299ac23bb8fee3a79a39
ninjaparekh25 <meetbscit@gmail.com>
2025-09-06 17:30:04 +0530

    cargo

---
+++ helloCargo/target/debug/deps/helloCargo-d2ce77191d5f029f.d
@@ -0,0 +1,5 @@
+/home/meet/Documents/Projects/Rust/helloCargo/target/debug/deps/helloCargo-d2ce77191d5f029f.d: src/main.rs
+
+/home/meet/Documents/Projects/Rust/helloCargo/target/debug/deps/helloCargo-d2ce77191d5f029f: src/main.rs
+
+src/main.rs:
---
+++ helloCargo/target/debug/helloCargo.d
@@ -0,0 +1,1 @@
+/home/meet/Documents/Projects/Rust/helloCargo/target/debug/helloCargo: /home/meet/Documents/Projects/Rust/helloCargo/src/main.rs
